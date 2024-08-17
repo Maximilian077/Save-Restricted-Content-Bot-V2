3e3e76802897c66851d71f3a8a1c4aaf376cd1a1
Maximilian077 <milanrana0777@gmail.com>
2024-08-17 13:45:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "28523160"))
+API_HASH = getenv("API_HASH", "4c3c1bc08310747eb8d52f9aadf82bcd")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
+OWNER_ID = int(getenv("OWNER_ID", "saverestrictedcontent0777Bot"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://Stranger_0777:IE7a7kW1bEoEuZNv@cluster0.tsbxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = int(getenv("LOG_GROUP", ""))
 FORCESUB = getenv("FORCESUB", "")
